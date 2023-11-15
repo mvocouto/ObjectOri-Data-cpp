fb9815732007fbb8dcb1c61c72f5cd3d9a4c4a07
Marcus Couto <mvo.couto@gmail.com>
2023-11-15 14:54:28 -0300

    second update

--- cube/cube.h
+++ cube/cube.h
@@ -1,4 +1,7 @@
-// .h codes start with this
+// .h codes start with this informatoin detailed below
+/* It is important to notice that the header file is used
+   to create the main variables and functions.*/
+
 #pragma once
 
 class Cube{
@@ -10,3 +13,7 @@ class Cube{
     private:
         double length_;
 };
+
+/* Please also notice that the variables and functions are divided into private and public variables.
+   The private variables must no be changed during the usage of the class, Probably
+   the structure of the langague would not permit as weel.*/
